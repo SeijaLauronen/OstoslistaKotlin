588c2a895e53e3136012845868540b41d9fbd095
Seija Lauronen <seija.lauronen@gmail.com>
2020-10-12 12:11:25 +0300

    ViewModenProviderin kanssa ongemia, kommentoitu siltä osin.

--- app/src/main/java/com/example/ostoslista/KategoryFragment.kt
+++ app/src/main/java/com/example/ostoslista/KategoryFragment.kt
@@ -1,11 +1,10 @@
 package com.example.ostoslista
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 
 //SSL TODO: Näitä tarviis:
@@ -23,6 +22,8 @@ import com.google.android.material.snackbar.Snackbar
 
 
 class KategoryFragment : Fragment() {
+
+    //private var kategoryViewModel: KategoryViewModel? = null //SSL 12.10.2020, kun tuo ViewModelProviders ei toiminut
 
     companion object {
         fun newInstance() = KategoryFragment()
@@ -45,6 +46,13 @@ class KategoryFragment : Fragment() {
         // 12.10.2020 Piti kommentoida, ettei kaadu:
         // 12.10.2020 SSL: tämä deprikoitunut : viewModel = ViewModelProviders.of(this).get(KategoryViewModel::class.java)
 
+        //onko nyt jotenkin eroa siinä muistivuotojutussa, kun tuolla Providers:lla ei pitänyt käyttää new:ta,
+        // että se new hanskataan automaattisesti?
+
+        //onko nyt jotenkin eroa siinä muistivuotojutussa, kun tuolla Providers:lla ei pitänyt käyttää new:ta,
+        // että se new hanskataan automaattisesti?
+
+        //val kategoryViewModel = ViewModelProvider(this).get<KategoryViewModel>(KategoryViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
